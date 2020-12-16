58b4b08cd1558b0a4383b30a38542f17e5460399
Jacob Allenwood <jacoballenwood@gmail.com>
2020-12-16 16:18:02 -0600

    chore: rename packages

---
+++ currency-formatter/src/androidTest/java/com/jacoballenwood/currency/CurrencyTextWatcherTest.kt
@@ -0,0 +1,38 @@
+package com.jacoballenwood.currency
+
+import android.widget.EditText
+import androidx.test.platform.app.InstrumentationRegistry
+import java.util.Currency
+import java.util.Locale
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class CurrencyTextWatcherTest {
+
+    @Test
+    fun testWatcher() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        val usLocale = Locale("en", "US")
+        val currency = Currency.getInstance(usLocale)
+        val symbol = "$"
+        val currencyFormatter =
+            CurrencyFormatter.getInstance(locale = usLocale, currency = currency, symbol = symbol)
+        val editText = EditText(context)
+        CurrencyTextWatcher(editText).apply {
+            setFormatter(currencyFormatter)
+            editText.addTextChangedListener(this)
+        }
+
+        editText.setText("25.00")
+        assertEquals("format was incorrect", editText.text.toString(), "$25.00")
+
+        editText.setText("5")
+        assertEquals("format was incorrect", editText.text.toString(), "$5")
+
+        editText.setText("0.05")
+        assertEquals("format was incorrect", editText.text.toString(), "$0.05")
+    }
+}
---
+++ currency-formatter/src/main/java/com/jacoballenwood/currency/SuperscriptSpanAdjuster.kt
@@ -0,0 +1,19 @@
+package com.jacoballenwood.currency
+
+import android.text.TextPaint
+import android.text.style.MetricAffectingSpan
+
+internal class SuperscriptSpanAdjuster(private val ratio: Float = 0.4f) : MetricAffectingSpan() {
+
+    override fun updateDrawState(tp: TextPaint?) {
+        tp?.updateBaseline()
+    }
+
+    override fun updateMeasureState(textPaint: TextPaint) {
+        textPaint.updateBaseline()
+    }
+
+    private fun TextPaint.updateBaseline() {
+        baselineShift += (ascent() * ratio).toInt()
+    }
+}
